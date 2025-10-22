d9a576101bcb12a7dcca9c84d8439a3ede5a621d
Yunyang-Fei <f.lucky@wustl.edu>
2025-10-22 13:44:47 -0500

    Fix geometricSum method to compute sum recursively
    Implemented recursive calculation of the geometric sum.

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -12,8 +12,11 @@ public class RecursiveMethods {
 	 *         ...)
 	 */
 	public static double geometricSum(int n) {
-		
-			// FIXME compute the geometric sum for the first n terms recursively
+		if (n==0) 
+			return 0;
+	    else 
+		return Math.pow(0.5,n)+geometricSum(n-1);
+		// FIXME compute the geometric sum for the first n terms recursively
 			return 0;
 		
 	}
